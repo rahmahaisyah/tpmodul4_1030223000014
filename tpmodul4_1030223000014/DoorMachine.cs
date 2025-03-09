23a858103953aabf096e07f9227de96bf602c158
rahmahaisyah <rahmahasyh@example.com>
2025-03-09 23:16:07 +0700

    "menambahkan implementasi dengan state-based construction"

---
+++ tpmodul4_1030223000014/DoorMachine.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_1030223000014
+{
+    class DoorMachine
+    {
+        public enum State { Terkunci, Terbuka }
+        private State currentState;
+
+        public DoorMachine()
+        {
+            currentState = State.Terkunci;
+            Console.WriteLine("Pintu terkunci");
+        }
+
+        public void BukaPintu()
+        {
+            if (currentState == State.Terkunci)
+            {
+                currentState = State.Terbuka;
+                Console.WriteLine("Pintu tidak terkunci");
+            }
+            else
+            {
+                Console.WriteLine("Pintu sudah terbuka");
+            }
+        }
+
+        public void KunciPintu()
+        {
+            if (currentState == State.Terbuka)
+            {
+                currentState = State.Terkunci;
+                Console.WriteLine("Pintu terkunci");
+            }
+            else
+            {
+                Console.WriteLine("Pintu sudah terkunci");
+            }
+        }
+    }
+}
--- tpmodul4_1030223000014/Program.cs
+++ tpmodul4_1030223000014/Program.cs
@@ -4,10 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Masukkan nama kelurahan: ");
-            string kelurahan = Console.ReadLine();
-            string kodePos = KodePos.getKodePos(kelurahan);
-            Console.WriteLine($"Kode Pos {kelurahan}: {kodePos}");
+            //Console.Write("Masukkan nama kelurahan: ");
+            //string kelurahan = Console.ReadLine();
+            //string kodePos = KodePos.getKodePos(kelurahan);
+            //Console.WriteLine($"Kode Pos {kelurahan}: {kodePos}");
+
+            DoorMachine door = new DoorMachine();
+
+            Console.Write("\nMasukkan perintah (BukaPintu / KunciPintu):");
+            string command = Console.ReadLine();
+
+            if (command == "BukaPintu")
+            {
+                door.BukaPintu();
+            }
+            else if (command == "KunciPintu")
+            {
+                door.KunciPintu();
+            }
+            else
+            {
+                Console.WriteLine("Perintah tidak dikenal.");
+            }
         }
     }
 }
