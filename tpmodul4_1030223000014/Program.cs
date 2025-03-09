e8962d007ea2e902acdacc6d315ff61a949e1889
rahmahaisyah <rahmahasyh@example.com>
2025-03-09 22:39:00 +0700

    Add project files.

---
+++ tpmodul4_1030223000014/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace tpmodul4_1030223000014
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
